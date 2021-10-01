e7149a44ae9777b8faa15f1738f85177a57f273c
amititme68 <amititme68@users.noreply.github.com>
2021-10-01 09:33:38 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.479c7eaad6179aa9ef0f326f88e9de59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90cbcc818a3aaed6434839b912bad68b",
+    "url": "https://amititme68.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "4b91df1296fee83663aa",
+    "url": "https://amititme68.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "0fdb9833db1d4910d6ca",
+    "url": "https://amititme68.github.io/task-management-frontend/static/js/2.80bb9459.chunk.js"
+  },
+  {
+    "revision": "4b91df1296fee83663aa",
+    "url": "https://amititme68.github.io/task-management-frontend/static/js/main.6465197a.chunk.js"
+  },
+  {
+    "revision": "9b1380d5356ae32bc045",
+    "url": "https://amititme68.github.io/task-management-frontend/static/js/runtime~main.99852ab9.js"
+  }
+]);
